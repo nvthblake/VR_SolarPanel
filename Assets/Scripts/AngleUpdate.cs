c93b8cf8d6ce7116f526fd7e648a7c3bc2c019d0
Nguyen <hvn28@drexel.edu>
2020-07-05 19:00:12 -0400

    First Commit

---
+++ Assets/Scripts/AngleUpdate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System;
+public class AngleUpdate : MonoBehaviour
+{
+    public Transform solarPanel;
+    public Text scoreText;
+    void Update()
+    {
+        scoreText.text = Math.Round(solarPanel.eulerAngles.x).ToString();
+    }
+}
